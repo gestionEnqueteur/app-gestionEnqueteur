36a2a37677710a7261c8723b52f1a6d3eb08c47d
Rémi PONCHE <ponche62880@gmail.com>
2025-02-25 22:25:14 +0100

    🥅 Catch the error on the handleRefresh

--- screens/TrainPlanifieScreen.tsx
+++ screens/TrainPlanifieScreen.tsx
@@ -4,19 +4,27 @@ import Course from "../models/Course";
 import { Text } from "react-native-paper";
 import { useStoreZustand } from "../store/storeZustand";
 import useSynchroApi from "../hook/useSynchroApi";
+import useSnackBar from "../hook/useSnackBar";
 
 export default function TrainPlanifieScreen() {
   const courses = useStoreZustand((state) => state.courses);
+  const snackbar = useSnackBar(); 
 
   const { synchroApiPush, synchroApiPull } = useSynchroApi();
 
   console.log(`mount TrainPlanfieScreen `);
 
   const handleOnRefresh = async () => {
-    console.log("refresh");
-    await synchroApiPush();
-    await synchroApiPull();
-    console.log("end refresh");
+    try {
+      console.log("refresh");
+      await synchroApiPush();
+      await synchroApiPull();
+      console.log("end refresh");
+
+    }
+    catch(error) {
+      snackbar({children: "Erreur de synchronisation ( réseau ?? )"}); 
+    }
   };
 
   const renderItem = ({ item }: { item: Course }) => {
