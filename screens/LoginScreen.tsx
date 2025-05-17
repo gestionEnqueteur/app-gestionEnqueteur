04c0fa01d3aefd08cf0014ccad5d610af15087a7
Rémi <ponche62880@gmail.com>
2025-05-17 16:19:29 +0200

    🐛 Fix bug spelling token_access and expoToken
    Fix the bug of the forgotten s for the jwt token. and unpack the expo push token

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -5,76 +5,87 @@ import useApi from "../hook/useApi";
 import { useStoreZustand } from "../store/storeZustand";
 import { AxiosError } from "axios";
 import useSnackBar from "../hook/useSnackBar";
-import {getExpoPushTokenAsync} from "expo-notifications";
+import { getExpoPushTokenAsync } from "expo-notifications";
 
 
 export default function LoginScreen() {
 
-  const [form, setForm ] = useState({identifier: "", password: ""}); 
-  const setUrlApi = useStoreZustand(state => state.setUrlApi); 
-  const urlApi = useStoreZustand(state => state.urlApi); 
+  const [form, setForm] = useState({ identifier: "", password: "" });
+  const setUrlApi = useStoreZustand(state => state.setUrlApi);
+  const urlApi = useStoreZustand(state => state.urlApi);
   const setJwt = useStoreZustand(state => state.setJwt)
-  const [isLoading, setIsLoading ] = useState(false);  
+  const [isLoading, setIsLoading] = useState(false);
 
-  const api = useApi(); 
-  const snackBar = useSnackBar(); 
+  const api = useApi();
+  const snackBar = useSnackBar();
 
   const handleOnSubmit = async () => {
     try {
       setIsLoading(true);
 
       const expoPushToken = await getExpoPushTokenAsync();
-      const responseApi = await api.post<{ acces_token: string}>('/auth/loginApp', {
+      console.log(expoPushToken)
+      const responseApi = await api.post<{ access_token: string }>('/auth/loginApp', {
         username: form.identifier,
         password: form.password,
-        expoPushToken: expoPushToken,
-      }); 
-      console.log(responseApi.data); 
+        expoPushToken: expoPushToken.data,
+      });
+      console.log(responseApi.data);
 
-      setJwt(responseApi.data.acces_token);
+      setJwt(responseApi.data.access_token);
 
-  
+
     }
     catch (error) {
-      console.error(error);
-      if (error instanceof AxiosError ) {
-        if(error.code === '400') {
-          snackBar({children: "Login incorrect"}); 
-        }
-        if(error.code === '429') {
-          snackBar({children: "Doucement avec le bouton, ne le casse pas"}); 
-        }
-        if(error.code === '500') {
-          snackBar({children: "Le serveur API à planté"}); 
-        }
+
+      if (!(error instanceof AxiosError)) {
+        snackBar({ children: "erreur inconnu" });
+        return;
+      }
+
+      switch (error.response?.status) {
+        case 401:
+          snackBar({ children: "Login incorrect" });
+          break;
+        case 400:
+          snackBar({ children: `Mauvaise request: ${error.response?.data?.message}`});
+          break;
+        case 429:
+          snackBar({ children: "Doucement avec le bouton, ne le casse pas" });
+          break;
+        case 500:
+          snackBar({ children: "Le serveur API à planté" });
+          break;
+        default:
+          snackBar({ children: `pas de réponse du Serveur` })
       }
     }
     finally {
       // the end 
-      setIsLoading(false);   
+      setIsLoading(false);
     }
-  } 
+  }
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Login</Text>
       <View style={styles.areaForm}>
-        <TextInput 
+        <TextInput
           label="Serveur API : "
           onChangeText={server => setUrlApi(server)}
           value={urlApi}
         />
-        <TextInput 
+        <TextInput
           label="Identifiant :"
-          onChangeText={identifier => setForm({...form, identifier})}
+          onChangeText={identifier => setForm({ ...form, identifier })}
         />
-        <TextInput 
+        <TextInput
           label="Mot de passe :"
-          onChangeText={password => setForm({...form, password})}
+          onChangeText={password => setForm({ ...form, password })}
           secureTextEntry
         />
 
-        <Button mode="contained" loading={isLoading} onPress={handleOnSubmit}>Valider</Button>
+        <Button mode="contained" loading={isLoading} onPress={handleOnSubmit} disabled={isLoading}>Valider</Button>
 
       </View>
     </View>
@@ -87,14 +98,14 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "space-around",
-    gap: 20, 
+    gap: 20,
   },
   title: {
-    marginTop: 70, 
+    marginTop: 70,
   },
-  areaForm: { 
-    flex: 2, 
+  areaForm: {
+    flex: 2,
     width: '80%',
-    gap: 20, 
+    gap: 20,
   }
 });
